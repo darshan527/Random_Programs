7198a2b7768a1ca424fbb2d82aaa8bc67d5974b5
darshan527 <darshan.hosamani6@gmail.com>
2021-04-01 23:59:59 +0530

    Test

--- Discrepancies in the Voters List-VOTERSDI.py
+++ Discrepancies in the Voters List-VOTERSDI.py
@@ -3,4 +3,18 @@
 d = {}
 a,b,c = list(map(int, input().split()))
 for _ in range(a+b+c):
-    
+    k = int(input())
+    if k in d:
+        d[k] += 1
+    else:
+        d[k] = 1
+nl = []
+c = 0
+for k,v in d:
+    if v > 1:
+        nl.append(k)
+        c +=1
+nl.sort()
+print(c)
+for i in nl:
+    print(i)

4ccd7d4d03f3cdc45fb001c49656f35522ccb3c1
darshan527 <darshan.hosamani6@gmail.com>
2021-04-16 23:24:37 +0530

    WA

--- Valentine Couples.py
+++ Valentine Couples.py
@@ -1,1 +1,11 @@
 # https://www.codechef.com/SPYB21C/problems/VCOUPLE
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    ma = 0
+    mb = 0
+    for i in range(n):
+        ma = max(ma, a[i])
+        mb = max(mb, b[i])
+    print(ma + mb)

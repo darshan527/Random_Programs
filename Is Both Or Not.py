ae16003f88925355ed17835d0b9d5841ed143ae7
darshan527 <darshan.hosamani6@gmail.com>
2021-05-05 23:51:04 +0530

    WIP

--- Is Both Or Not.py
+++ Is Both Or Not.py
@@ -1,7 +1,7 @@
 n = int(input())
-if (n % 5) == 0 or (n % 11) == 0:
-    print("ONE")
-elif (n % 5) == 0 and (n % 11) == 0:
+if (n % 5) == 0 and (n % 11) == 0:
     print("BOTH")
+elif (n % 5) == 0 or (n % 11) == 0:
+    print("ONE")
 else:
     print("NONE")
